855f8efd60042061e2bb000281d3306092898c07
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-09-07 01:48:15 +0900

    design: install react icon, InputSearch컴포넌트 레이아웃

---
+++ src/components/inputSearch/InputSearch.styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledInputSearch = styled.div`
+  width: 80%;
+  background-color: white;
+  border-radius: 50%;
+  padding: 15px 10px;
+`;
---
+++ src/components/inputSearch/InputSearch.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyledInputSearch } from './InputSearch.styled';
+
+const InputSearch = () => {
+  return (
+    <StyledInputSearch>
+      <input type='text' defaultValue='' />
+      <button></button>
+    </StyledInputSearch>
+  );
+};
+
+export default InputSearch;
